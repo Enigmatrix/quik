b90af47f1d11f897bd1ca10c28a646be19ee85ed
Enigmatrix <enigmatrix2000@gmail.com>
2024-09-29 03:44:45 +0800

    fix: wrong byte order in packet parsing

--- quecto-quic/src/transport.rs
+++ quecto-quic/src/transport.rs
@@ -17,27 +17,31 @@ impl Connection {
 
     pub fn recv(&self, mut data: &[u8]) -> Result<()> {
         let first_byte = data.read_u8()?;
-        if first_byte & 0b1 == 1 {
+        // Header Form (1) bit
+        if first_byte >> 7 != 0 {
             // Long Header
+            // Fixed Bit (1) = 1 - ignored
+            // Long Packet Type (2) - not used in VersionNegotiation
+            let packet_type = (first_byte >> 4) & 0b11;
+            // Reserverd (2) - ignored
+            // Packet Number Length (2) - not used in Retry & VersionNegotiation
+            let packet_number_length = first_byte & 0b11;
+
             let version = data.read_u32::<NetworkEndian>()?;
             let dest_conn_id = ConnectionId::parse(&mut data)?;
             let src_conn_id = ConnectionId::parse(&mut data)?;
-            if (first_byte >> 1) & 0b1 == 0 {
-                if version == 0 {
-                    // VersionNegotiation packet
-                } else {
-                    // throw error, this is invalid
-                }
+
+            if version == 0 {
+                // VersionNegotiation packet
+                return Ok(())
             }
-            let packet_number_length = first_byte >> 6; // not always needed
-            let packet_type = (first_byte >> 2) & 0b11;
             match packet_type {
                 0b00 => {
                     // Initial packet
                     let token_length = VarInt::parse(&mut data)?;
                     let (token, mut data) = data
                         .split_at_checked(token_length.into())
-                        .ok_or("Token too long")?;
+                        .ok_or_else(|| "Token too long")?;
                     let packet_number =
                         PacketNumber::parse(&mut data, 1 + packet_number_length as usize)?;
                     self.handle_initial_packet(InitialPacket {
@@ -47,6 +51,7 @@ impl Connection {
                         token,
                         packet_number,
                     })?;
+                    // TODO there are multiple frames...
                     self.handle_raw_frame(data)?;
                 }
                 0b01 => {
